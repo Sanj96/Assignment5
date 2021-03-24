08829c9c593c0970eebeb08a8c8a4a3fe650796b
Sanj96 <71392728+Sanj96@users.noreply.github.com>
2021-03-23 23:16:17 -0400

    Update DEX.sol

--- DEX.sol
+++ DEX.sol
@@ -2,7 +2,8 @@
 
 pragma solidity ^0.6.10;
 
-import “./SafeYorkERC20Token.sol”;
+import "https://github.com/Sanj96/Assignment5/blob/main/SafeYorkERC20Token.sol";
+
 contract DEX {
 
     IERC20 public token;
@@ -15,23 +16,48 @@ contract DEX {
     }
     
     function buy() payable public {
-        // TODO
-        uint256 amountTobuy = msg.value;
+        
+           //amount to buy
+            uint256 amountTobuy = msg.value;
+        
+      	    //token balance
 	    uint256 dexBalance = token.balanceOf(address(this));
+	    
+	    //Transaction should be greater than 0 
 	    require(amountTobuy > 0, "You need to send some ether");
+	    
+	    //Checking if enough tokens are available
 	    require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+	    
+	     // transfer token to caller
 	    token.transfer(msg.sender, amountTobuy);
+	    
+	    //emit the Transfer and Bought events
 	    emit token.Transfer(address(this), msg.sender, amountTobuy);
 	    emit Bought(amountTobuy);
     }
     
     function sell(uint256 amount) public {
-        // TODO
-        require(amount > 0, "You need to sell at least some tokens");
-        uint256 allowance = token.allowance(msg.sender, address(this));
+        
+            //Transaction of tokens should be greater than 0 
+             require(amount > 0, "You need to sell at least some tokens");
+        
+            //Allowance 
+            uint256 allowance = token.allowance(msg.sender, address(this));
+        
+            //check if allowance is greater than amount of tokens to sell
 	    require(allowance >= amount, "Check the token allowance");
+	    
+	    //approval of transaction 
+	    token.approve(msg.sender, _amount);
+	    
+	    //Checking if transfer is successful from caller address to contract address
 	    token.transferFrom(msg.sender, address(this), amount);
+	    
+	    // send ether to caller
 	    msg.sender.transfer(amount);
+	    
+	    //emit the Transfer and Sold events
 	    emit token.Transfer(msg.sender, address(this),amount);
 	    emit Sold(amount);
     }
